264a9de896cbd665b844b605335a33b6547c4f35
Hiren K Vaghasiya <vaghasiyaharryk@gmail.com>
2023-04-02 04:09:07 +0400

    Minor CSS Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,14 +5,34 @@ const Body = () => {
     return (
         <main className="main__content">
             <div className="filter__container">
-                <button
-                    className="filter__toprated__btn"
-                    onClick={() => {
-                        console.log("Filter");
-                    }}
-                >
-                    Top Rated Restaurant
-                </button>
+                <div className="filter__row">
+                    <button
+                        className="filter__btn"
+                        onClick={() => {
+                            console.log("Filter");
+                        }}
+                    >
+                        Relevance
+                    </button>
+
+                    <button
+                        className="filter__btn"
+                        onClick={() => {
+                            console.log("Filter");
+                        }}
+                    >
+                        Top Rated Restaurant
+                    </button>
+
+                    <button
+                        className="filter__btn"
+                        onClick={() => {
+                            console.log("Filter");
+                        }}
+                    >
+                        Fast Delivery Time
+                    </button>
+                </div>
                 <input type="text" className="search" placeholder="Search" />
             </div>
             <div className="res__container res__card__container">
