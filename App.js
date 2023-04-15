fdde79dfaaa598690f77adfd477a7b899192c804
Hiren K Vaghasiya <vaghasiyaharryk@gmail.com>
2023-04-15 23:47:43 +0400

    Footer update

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from './src/components/Body';
+import Footer from "./src/components/Footer";
 
 const App = () => {
     return (
         <>
             <Header />
             <Body />
+            <Footer />
         </>
     );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            {/* author credit */}
+            <p className="author-footer">
+                Made with 💝 by&nbsp;
+                <a href="https://github.com/hirenkvaghasiya" target="_blank" rel="noopener noreferrer">
+                    Hiren Vaghasiya
+                </a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
